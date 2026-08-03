cc366c5b84a869c56f21d4c74e2cae854b1d6319
doctrades <trade.namanrajsingh@gmail.com>
2026-08-03 18:44:16 +0530

    Update data.js

--- data.js
+++ data.js
@@ -10,7 +10,7 @@
 const CONFIG = {
     // 1 USD = this many INR. Update this constant whenever you
     // want to refresh the peg used for the currency toggle.
-    usdToInr: 95.39,
+    usdToInr: 98.22,
 
     fund: {
         name: "Doc Trades Hedge Fund",
