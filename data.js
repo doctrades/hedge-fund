15910d2dad408eb9efaf9d07c5623bde870949bd
doctrades <trade.namanrajsingh@gmail.com>
2026-08-05 04:06:39 +0530

    Update data.js

--- data.js
+++ data.js
@@ -34,4 +34,7 @@ const LEADERBOARD = [
     { id: "INV-YR", name: "Yash", investedUSD: 30, payoutGiven: false },
     { id: "INV-SP", name: "Snehil", investedUSD: 30, payoutGiven: false },
     { id: "INV-SG", name: "Shiva", investedUSD: 30, payoutGiven: false },
+     { id: "INV-GK", name: "Gitesh", investedUSD: 30, payoutGiven: false },
+     { id: "INV-NS", name: "Naman Raj", investedUSD: 60, payoutGiven: false },
+     { id: "INV-ND", name: "Naman Dhw", investedUSD: 60, payoutGiven: false },
 ];
